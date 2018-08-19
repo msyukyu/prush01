0aa9033e7d49c968e612b4ecfaabcce384b41500
Damien Abeloos <dabeloos@w-r10-p4.s19.be>
2018-08-19 19:50:27 +0200

    current work, norm

--- ex00/solver.c
+++ ex00/solver.c
@@ -5,14 +5,15 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/08/19 08:16:04 by dabeloos          #+#    #+#             */
-/*   Updated: 2018/08/19 18:23:58 by dabeloos         ###   ########.fr       */
+/*   Created: 2018/08/19 19:45:50 by dabeloos          #+#    #+#             */
+/*   Updated: 2018/08/19 19:49:37 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "handler.h"
 #include "restrictor.h"
+#include "solver.h"
 
 int						ft_check_fullness(struct s_sudoku_grid *grid)
 {
@@ -32,14 +33,10 @@ int						ft_check_fullness(struct s_sudoku_grid *grid)
 	return (1);
 }
 
-struct s_sudoku_grid	*ft_place_min_stack(struct s_sudoku_grid *grid, 
-		int count)
+void					ft_min_stack(struct s_sudoku_grid *grid, int var[])
 {
-	int var[5];
-	int	l;
+	int l;
 	int c;
-	struct s_sudoku_grid	*newgrid;
-	struct s_sudoku_grid	*safeguard;
 
 	var[1] = 11;
 	l = -1;
@@ -62,38 +59,53 @@ struct s_sudoku_grid	*ft_place_min_stack(struct s_sudoku_grid *grid,
 			}
 		}
 	}
+}
+
+struct s_sudoku_grid	*ft_place_number(struct s_sudoku_grid *grid,
+		int count, int var[], int l)
+{
+	struct s_sudoku_grid	*safeguard;
+	struct s_sudoku_grid	*newgrid;
+
+	newgrid = ft_copy_grid(grid);
+	safeguard = newgrid;
+	newgrid->sudoku[var[2]][var[3]][0] = newgrid->sudoku[var[2]][var[3]][l];
+	newgrid = ft_check_line(newgrid, var[2]);
+	if (newgrid != (void*)0)
+	{
+		newgrid = ft_check_column(newgrid, var[3]);
+		if (newgrid != (void*)0)
+		{
+			newgrid = ft_check_subgrid(newgrid, var[2], var[3]);
+			if (newgrid != (void*)0)
+			{
+				count += ft_check_fullness(newgrid);
+				if (count == 1)
+					return (newgrid);
+				newgrid = ft_place_min_stack(newgrid, count);
+				if (newgrid == (void*)0)
+					free(safeguard);
+			}
+		}
+	}
+	return (newgrid);
+}
+
+struct s_sudoku_grid	*ft_place_min_stack(struct s_sudoku_grid *grid,
+		int count)
+{
+	int						var[4];
+	int						l;
+	struct s_sudoku_grid	*newgrid;
+
+	ft_min_stack(grid, var);
 	if (var[1] == 0)
 		return ((void*)0);
 	else
 	{
 		l = 0;
 		while (++l < var[1])
-		{
-			newgrid = ft_copy_grid(grid);
-			safeguard = newgrid;
-			newgrid->sudoku[var[2]][var[3]][0] = 
-				newgrid->sudoku[var[2]][var[3]][l];
-			newgrid = ft_check_line(newgrid, var[2]);
-			if (newgrid != (void*)0)
-			{
-				newgrid = ft_check_column(newgrid, var[3]);
-				if (newgrid != (void*)0)
-				{
-					newgrid = ft_check_subgrid(newgrid, var[2], var[3]);
-					if (newgrid != (void*)0)
-					{
-						count += ft_check_fullness(newgrid);
-						if (count == 1)
-							return (newgrid);
-						newgrid = ft_place_min_stack(newgrid, count);
-						if (newgrid == (void*)0)
-						{
-							free(safeguard);
-						}
-					}
-				}	
-			}
-		}
+			newgrid = ft_place_number(grid, count, var, l);
 	}
 	return (newgrid);
 }
--- ex00/solver.h
+++ ex00/solver.h
@@ -6,7 +6,7 @@
 /*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/19 08:24:34 by dabeloos          #+#    #+#             */
-/*   Updated: 2018/08/19 18:24:23 by dabeloos         ###   ########.fr       */
+/*   Updated: 2018/08/19 19:49:07 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,9 @@
 # define SOLVER_H
 
 int						ft_check_fullness(struct s_sudoku_grid *grid);
+void					ft_min_stack(struct s_sudoku_grid *grid, int var[]);
+struct s_sudoku_grid	*ft_place_number(struct s_sudoku_grid *grid, int count,
+		int var[], int l);
 struct s_sudoku_grid	*ft_place_min_stack(struct s_sudoku_grid *grid,
-		int count);
+		int	count);
 #endif
